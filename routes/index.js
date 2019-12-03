e64424b37da03e3095c6f6dce87e846cc5e36a8b
SaumyaShastri <saumyashastri@gmail.com>
2019-12-03 13:18:55 -0500

    Randomly picking questions and displaying

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,31 @@
-
 const questions = require("../public/question");
-//const easy = questions.easy
+const easy = questions.easy;
+// const num = Math.floor(Math.random() * easy.length);
+// console.log(num);
+// console.log(easy.length);
+
+const qkey = getRandom(easy,3);
+
+//Picking random 3 easy questions
+function getRandom(arr, n) {
+  var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+  if (n > len)
+      throw new RangeError("getRandom: more elements taken than available");
+  while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+}
+
 const constructorMethod = app => {
 
     app.get("*", async (req, res) => {     
       console.log(questions)
-      res.render("layouts/main", questions.easy);
+      res.render("layouts/main", qkey);
 
     });
   };
