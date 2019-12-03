a6a0b0bc57bcc5a0fe2be840a32b1a23ae61f960
Saumya Shastri <saumyashastri@gmail.com>
2019-12-03 12:10:39 -0500

    Add files via upload
    Entry point to the quiz feature

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const static = express.static(__dirname + "/public");
+
+const configRoutes = require("./routes");
+const exphbs = require("express-handlebars")//.create({layoutsDir:path.join(__dirname, '/views/layouts') , defaultLayout: 'main'});
+
+app.use("/public", static);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+});
